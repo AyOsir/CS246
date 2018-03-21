0b6525b7abf7bd6fdee504fb2378fdc2e1143695
Greg56 <32373129+Greg56@users.noreply.github.com>
2018-03-20 22:58:06 -0400

    Update bag.h

--- Bag Class/bag.h
+++ Bag Class/bag.h
@@ -1,1 +1,52 @@
-#ifndef BAG_H
+#include <iostream>
+#define MAX_SIZE 20
+using namespace std;
+
+class Bag {
+private:
+int numbers[MAX_SIZE]; // LIST OF NUMBERS
+int currentSize; // current size of the list
+
+public:
+Bag() {
+currentSize = 0; // set the current size to 0
+}
+
+// parametrized constructor
+Bag(int arr[], int size){  
+if(size > MAX_SIZE) {
+cout << "Max size 20 allowed..." << endl;
+return;
+}
+for(int i=0; i<size; i++)
+numbers[i] = arr[i];
+currentSize = size;
+}
+
+// add a number to bag
+void addToBag(int n) {
+if(currentSize == MAX_SIZE) {
+cout << "Bag is already full..." << endl;
+return;
+}
+numbers[currentSize++] = n;
+}
+  
+// get the bag numbers
+int* getNumbers(){
+return numbers;
+}
+
+// get bag's current size
+int getSize() {
+return currentSize;
+}
+
+  
+// print the bag numbers
+void display() {
+for(int i=0; i<currentSize; i++)
+cout << numbers[i] << " ";
+cout << endl;
+}
+};
